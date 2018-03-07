0bfad1b378416c7308bc26c2c301c109f9521c63
takahiro <nobooknolife16@gmail.com>
2018-03-07 17:24:22 +0900

    finish 2-8

---
+++ lesson7-2.rb
@@ -0,0 +1,19 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+c = gets.to_i
+d = 1
+while d <= c do
+	puts "#{d}回目の計算"
+	puts "2つの値を入力してください"
+	a = gets.to_i
+	b = gets.to_i
+	puts "a=#{a}"
+	puts "b=#{b}"
+	puts "計算結果を出力します"
+	puts "a+b=#{a+b}"
+	puts "a-b=#{a-b}"
+	puts "a*b=#{a*b}"
+	puts "a/b=#{a/b}"
+	d += 1
+end
+puts "計算を終了します"
